1de71977ad7c0550dac9a83cd3ea8c2d35a828c8
Salih Bedelce <salih_bedelce@hotmail.de>
2025-11-14 23:48:55 +0100

    fix

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -4,7 +4,15 @@ import './GamePage.css'
 const GamePage = () => {
   return (
     <div className="game-page page-with-bottom-nav">
-      <main className="game-main" />
+      <main className="game-main">
+        <div className="game-content">
+          <img
+            src="./assets/Schatztruhe.png"
+            alt="Schatztruhe"
+            className="treasure-image"
+          />
+        </div>
+      </main>
       <BottomNavigation />
     </div>
   )
